871adaf5a258da4f55d41507e81540143c40aed5
Phlaesh <114645175+Phlaesh@users.noreply.github.com>
2023-10-04 10:21:18 +0200

    Add files via upload

---
+++ trollo/Model/BoardContext.cs
@@ -0,0 +1,27 @@
+﻿// TodoContext.cs
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Model
+{
+    public class BoardContext : DbContext
+    {
+        public DbSet<Board> Boards { get; set; }
+        public string DbPath { get; }
+
+        public BoardContext()
+        {
+            DbPath = "bin/Todos.db";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Board>().ToTable("Boards");
+        }
+    }
+}
+

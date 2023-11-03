2aa73daba9794441369d2c630c04cd79e1da38fb
Phlaesh <114645175+Phlaesh@users.noreply.github.com>
2023-11-03 08:37:45 +0100

    Delete modul11/SortedLinkedList directory

--- modul11/SortedLinkedList/User.cs
+++
@@ -1,9 +0,0 @@
-public class User {
-    public string Name { get; set; }
-    public int Id { get; set; }
-
-    public User (string name, int id) {
-        this.Name = name;
-        this.Id = id;
-    }
-}
